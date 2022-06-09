81ddcc3ad35a6dda72893db8071856b36216536b
Kavindu Piyumal <piyumal27100@gmail.com>
2022-06-09 08:34:21 +0530

    update create function

--- api/controllers/TodolistController.js
+++ api/controllers/TodolistController.js
@@ -11,9 +11,16 @@ module.exports = {
   async create(req,res){
       try{
         let params= req.allParams();
-        if(!params.taskName){
-          return res.badRequest({err:'name is required'});
+        if(!params.taskName && !params.hours){
+            return res.badRequest({err:'taskName and hours is required'});
+          }
+        else if(!params.taskName){
+          return res.badRequest({err:'taskName is required'});
         }
+        else if(!params.hours){
+            return res.badRequest({err:'hours is required'});
+        }
+   
         const results= await Todolist.create({
             taskName:params.taskName,
             hours:params.hours,
